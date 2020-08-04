380cc10f2f22b03e16adfe3a71067e3007d3cc55
Kristofer <kristoferhenriquez@gmail.com>
2020-08-03 20:26:30 -0500

    created index action and showed images

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="home">
-    sdvsdsdfsdfsdff
-    <img alt="Vue logo" src="../assets/logo.png">
-    sdsdsdsdsd
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    sdsdsdsdsdsdsd
-  </div>
 
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p> name: {{ product.name }} </p>
+      <p> description {{ product.description }} </p>
+      <p> image: {{ product.image_url }} </p>
+      <img v-bind:src="product.image_url">
+      <hr>
+    </div>
+
+  </div>
 </template>
-sdsdsdsdsd
+<style>
+</style>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function() {
+      console.log("This will be the index...");
+
+      axios.get("/api/products").then(response => {
+        console.log(response);
+        this.products = response.data;
+      });
+
+    }
   }
-}
+};
 </script>
