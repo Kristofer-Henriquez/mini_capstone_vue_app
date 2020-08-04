ea8516dbfe111e9420c172a9f2c2830591560ddb
Kristofer <kristoferhenriquez@gmail.com>
2020-08-03 19:31:47 -0500

    initial comit, testing vue with a cloned app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
+    sdvsdsdfsdfsdff
     <img alt="Vue logo" src="../assets/logo.png">
+    sdsdsdsdsd
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    sdsdsdsdsdsdsd
   </div>
-</template>
 
+</template>
+sdsdsdsdsd
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
