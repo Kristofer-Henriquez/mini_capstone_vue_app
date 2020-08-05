c80f9f3bc15f85e41a8be4f58d49c9ff3b878886
Kristofer <kristoferhenriquez@gmail.com>
2020-08-04 20:08:24 -0500

    created the Show action and reworked the Create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
-<!-- Here's where the index stuff starts -->
+<!-- Here's where the create stuff starts -->
 
     <h1>{{ message }}</h1>
     <p>Click below to add a product to the database!</p>
+    <p>Name: <input v-model="newProductName" type="text"></p>
+    <p>Description: <input v-model="newProductDescription" type="text"></p>
+    <p>Price: <input v-model="newProductPrice" type="text"></p>
+    <p>Image_url: <input v-model="newProductImageUrl" type="text"></p>
     <button v-on:click="createProduct()"> Product Creation! </button>
 
-<!-- And here's where the create stuff starts -->
+<!-- And here's where the index stuff starts -->
 
     <h1> See All Products Below </h1>
     <div v-for="product in products">
@@ -14,9 +18,29 @@
       <p> description {{ product.description }} </p>
       <p> price: {{ product.price }} </p>
       <p> image: {{ product.image_url }} </p>
+      <button v-on:click="showProduct(product)"> Show more info </button>
+      <br>
+      <br>
+      <br>
+      <br>
+      
       <img v-bind:src="product.image_url">
       <hr>
     </div>
+
+    <!-- Here's where the show info starts -->
+
+    <dialog id="product-details">
+      <form method="dialog">
+        <h1>Product Info</h1>
+        <p>Name: {{currentProduct.name}}</p>
+        <p>Description: {{currentProduct.description}} </p>
+        <p>Price: {{currentProduct.price}}</p>
+        <p>Image_url: {{currentProduct.image_url}} </p>
+        <button> Close </button>
+      </form>
+    </dialog>
+
 
   </div>
 </template>
@@ -29,7 +53,12 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      products: []
+      products: [],
+      newProductName: "",
+      newProductDescription: "",
+      newProductPrice: "",
+      newProductImageUrl: "",
+      currentProduct: [],
     };
   },
   created: function() {
@@ -49,16 +78,22 @@ export default {
       console.log("This will create a product..");
 
       var params = {
-        name: "Banana",
-        description: "It's... a banana",
-        price: 6,
-        image_url: "https://media.makeameme.org/created/im-a-banana-5cb0a4.jpg"
+        name: this.newProductName,
+        description: this.newProductDescription,
+        price: this.newProductPrice,
+        image_url: this.newProductImageUrl
       };
     
       axios.post("/api/products", params).then(response => {
         console.log(response.data);
         this.products.push(response.data);
       });
+    },
+
+    showProduct: function(product) {
+      console.log(product);
+      this.currentProduct = product;
+      document.querySelector("#product-details").showModal();
     }
   },
   
